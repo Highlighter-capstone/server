84cea95fb02138a2e69e81c9bc5546244ecc19d3
qjatjr29 <qjatjr29@khu.ac.kr>
2022-04-20 01:32:06 +0900

    [Refactor] : 충돌 해결

--- service/detecting_service.py
+++ service/detecting_service.py
@@ -1,6 +1,7 @@
 import os
 from yolo.yolov5 import detect
 
+
 def get_frames(file_name):
     # frame들 이름 가져오기
     dir_path = "../datasets/frame"
@@ -8,7 +9,8 @@ def get_frames(file_name):
     max_value = -1
     for root, directories, files in os.walk(dir_path):
         for file in files:
-            max_value = max(int(file.split("frame")[1].split(".")[0]), max_value)
+            max_value = max(
+                int(file.split("frame")[1].split(".")[0]), max_value)
     for i in range(max_value + 1):
         frames.append(os.path.join(dir_path, (file_name + str(i) + ".jpg")))
     return frames
@@ -16,51 +18,51 @@ def get_frames(file_name):
 # 하이라이트 시간 추출
 def get_highlight_times():
     frames = get_frames()
-    n=0
-    left=-1
-    right=-1
-    buff=0
-    list=[]
+    n = 0
+    left = -1
+    right = -1
+    buff = 0
+    list = []
 
-    #장면마다
+    # 장면마다
     for frame in frames:
-        #웃는얼굴찾기 실패
+        # 웃는얼굴찾기 실패
         if not(detect_people(frame)):
-            #버퍼큐에 있는데 실패중
-            if right!=-1:
-                buff+=1
-            #버퍼큐에 있는데 3초동안 웃는얼굴 안나옴
-            if buff==3:
-                temp=[]
-                buff=0
-                #초반 2초
-                if left<2:
+            # 버퍼큐에 있는데 실패중
+            if right != -1:
+                buff += 1
+            # 버퍼큐에 있는데 3초동안 웃는얼굴 안나옴
+            if buff == 3:
+                temp = []
+                buff = 0
+                # 초반 2초
+                if left < 2:
                     temp.append(0)
                     temp.append(right)
                 else:
                     temp.append(left-2)
                     temp.append(right)
-                left=-1
-                right=-1
+                left = -1
+                right = -1
                 list.append(temp)
             continue
-        #웃는얼굴 찾음
+        # 웃는얼굴 찾음
         else:
-            #버퍼큐에 없음
-            if left==-1: 
-                left=n
-                right=n
-            #버퍼큐에 있음
+            # 버퍼큐에 없음
+            if left == -1:
+                left = n
+                right = n
+            # 버퍼큐에 있음
             else:
-                right=n
-            buff=0
-        n+=1
+                right = n
+            buff = 0
+        n += 1
     return list
 
+    # Buffer Queue 적용
 
 
-
-#yolov5
+# yolov5
 def detect_people(file_name):
     result = detect.run(weights="../yolo/yolov5/best.pt",
                         source="../datasets/frame/" + str(file_name))
@@ -70,9 +72,15 @@ def detect_people(file_name):
         return False
 
 
-#Haar Cascade
+# Haar Cascade
 def detect_smile(ob):
-    
+    return False
+
+
+
+# yolo test
+# for i in range(18):
+#     detect_people("concert" + str(i) + ".jpg")
 
 
 #yolo test
